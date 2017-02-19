150bf9c41c20ab084b3578cc0bd584a3b399307d
Sarah Olson <solson@trello.com>
2017-02-19 10:24:22 -0600

    Phone UI prep

---
+++ WorkoutTracker/WorkoutTracker/HeartRateTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HeartRateTableViewController.swift
+//  WorkoutTracker
+//
+//  Created by Sarah Olson on 2/19/17.
+//  Copyright © 2017 HPlus. All rights reserved.
+//
+
+import UIKit
+
+class HeartRateTableViewController: UITableViewController {
+
+    var heartRateData: [String] = ["63", "87"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heartRateData.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "heartRate", for: indexPath)
+        cell.textLabel?.text = heartRateData[indexPath.row]
+        return cell
+    }
+}
